43c9d357017b16a0877ad45dc2a35ed82c682edd
alkeity <alkeity.23@gmail.com>
2024-08-02 01:16:21 +0300

    feat: option removal

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -6,10 +6,9 @@ CONST CHAR* VALUES[] = { "Zero", "One", "Two", "Three", "Four", "Five" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG2), NULL, (DLGPROC)DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG_LIST), NULL, (DLGPROC)DlgProc, 0);
 	return 0;
 }
 
@@ -28,22 +27,48 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case WM_COMMAND:
-		switch (LOWORD(wParam))
 		{
-		case IDOK:
-		{
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST_BOX);
 			CONST INT SIZE = 256;
 			CHAR szBuffer[SIZE]{};
 			CHAR szMessage[SIZE]{};
-			HWND hListBox = GetDlgItem(hwnd, IDC_LIST_BOX);
-			INT index = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
-			SendMessage(hListBox, LB_GETTEXT, index, (LPARAM)szBuffer);
-			sprintf_s(szMessage, "You choose №%i (%s)", index, szBuffer);
-			MessageBox(hwnd, szMessage, "Your choice", MB_OK | MB_ICONINFORMATION);
-		}
+
+			switch (LOWORD(wParam))
+			{
+			case IDC_BUTTON_ADD:
+			{
+				// dialog box that requests value to added item, add item
+			}
 			break;
-		case IDCANCEL:
-			EndDialog(hwnd, 0);
+			case IDC_BUTTON_REMOVE:
+				// just remove selected item
+			{
+				INT index = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+				SendMessage(hListBox, LB_DELETESTRING, (WPARAM)index, 0);
+				SendMessage(hListBox, LB_SETCURSEL, 0, 0);
+			}
+			break;
+			case IDC_CHECK_SORT:
+				// sort on/off (????)
+			{
+				//
+			}
+				break;
+			case IDOK:
+			{
+				INT index = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+				if (index == CB_ERR) MessageBox(hwnd, "You need to make a choice", "Nah", MB_OK | MB_ICONWARNING);
+				else
+				{
+					SendMessage(hListBox, LB_GETTEXT, index, (LPARAM)szBuffer);
+					sprintf_s(szMessage, "You choose №%i (%s)", index, szBuffer);
+					MessageBox(hwnd, szMessage, "Your choice", MB_OK | MB_ICONINFORMATION);
+				}
+			}
+			break;
+			case IDCANCEL:
+				EndDialog(hwnd, 0);
+			}
 		}
 		break;
 	case WM_CLOSE:
--- WinAPI/ListBox/resource.h
+++ WinAPI/ListBox/resource.h
@@ -2,18 +2,24 @@
 // Microsoft Visual C++ generated include file.
 // Used by ListBox.rc
 //
-#define IDD_DIALOG1                     101
-#define IDD_DIALOG2                     101
+#define IDD_DIALOG_LIST                 101
+#define IDD_DIALOG_INPUT                103
+#define IDD_DIALOG_OPTION               103
 #define IDC_LIST_BOX                    1001
 #define IDC_STATIC_CAPTION              1002
+#define IDC_BUTTON_ADD                  1003
+#define IDC_BUTTON_REMOVE               1004
+#define IDC_CHECK_SORT                  1005
+#define IDC_EDIT_OPTION                 1006
+#define IDC_STATIC_OPTION               1007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
