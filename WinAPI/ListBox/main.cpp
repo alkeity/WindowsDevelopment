bfca08569c51fff329697a2e9560d4d49585ae59
alkeity <alkeity.23@gmail.com>
2024-08-01 14:51:40 +0300

    feat: ListBox

---
+++ WinAPI/ListBox/main.cpp
@@ -0,0 +1,54 @@
+﻿#include <Windows.h>
+#include<cstdio>
+#include "resource.h"
+
+CONST CHAR* VALUES[] = { "Zero", "One", "Two", "Three", "Four", "Five" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG2), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hListBox = GetDlgItem(hwnd, IDC_LIST_BOX);
+		for (size_t i = 0; i < sizeof(VALUES) / sizeof(VALUES[0]); i++)
+		{
+			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)VALUES[i]);
+		}
+		SendMessage(hListBox, LB_SETCURSEL, 0, 0);
+	}
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR szBuffer[SIZE]{};
+			CHAR szMessage[SIZE]{};
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST_BOX);
+			INT index = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			SendMessage(hListBox, LB_GETTEXT, index, (LPARAM)szBuffer);
+			sprintf_s(szMessage, "You choose №%i (%s)", index, szBuffer);
+			MessageBox(hwnd, szMessage, "Your choice", MB_OK | MB_ICONINFORMATION);
+		}
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+
+	return FALSE;
+}
---
+++ WinAPI/ListBox/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ListBox.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     101
+#define IDC_LIST_BOX                    1001
+#define IDC_STATIC_CAPTION              1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
