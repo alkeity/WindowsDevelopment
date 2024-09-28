f00618f6c93d75fa83c2393290ccedc6fae9e976
alkeity <alkeity.23@gmail.com>
2024-09-28 17:49:56 +0300

    feat: move Alarm class to separate file

---
+++ WindowsForms/Clock/Alarm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+	public class Alarm : IComparable
+	{
+		DateTime alarmTime;
+		string soundPath;
+
+		public DateTime AlarmTime
+		{
+			get => alarmTime;
+			set
+			{
+				if (DateTime.Now < value) alarmTime = value;
+				else throw new ArgumentOutOfRangeException($"Can't set alarm in the past. Alarm time: {value}.");
+			}
+		}
+
+		public string SoundPath
+		{
+			get => soundPath;
+			set
+			{
+				if (File.Exists(value)) soundPath = value;
+				else throw new FileNotFoundException($"File {value} not found.");
+			}
+		}
+
+		public Alarm()
+		{
+			alarmTime = DateTime.Now;
+			soundPath = string.Empty;
+		}
+
+		public int CompareTo(object obj)
+		{
+			return alarmTime.CompareTo((obj as Alarm).AlarmTime);
+		}
+
+		public override string ToString()
+		{
+			return $"{alarmTime}, {soundPath.Split('\\').Last()}";
+		}
+	}
+}
--- WindowsForms/Clock/AlarmDialogue.cs
+++ WindowsForms/Clock/AlarmDialogue.cs
@@ -48,41 +48,4 @@ namespace Clock
 			if (ofd.ShowDialog() == DialogResult.OK) alarm.SoundPath = ofd.FileName;
 		}
 	}
-
-	public class Alarm : IComparable
-	{
-		DateTime alarmTime;
-		string soundPath;
-
-		public DateTime AlarmTime
-		{
-			get => alarmTime;
-			set
-			{
-				if (DateTime.Now < value) alarmTime = value;
-				else throw new ArgumentOutOfRangeException($"Can't set alarm in the past. Alarm time: {value}.");
-			}
-		}
-
-		public string SoundPath
-		{
-			get => soundPath;
-			set
-			{
-				if (File.Exists(value)) soundPath = value;
-				else throw new FileNotFoundException($"File {value} not found.");
-			}
-		}
-
-		public Alarm()
-		{
-			alarmTime = DateTime.Now;
-			soundPath = string.Empty;
-		}
-
-		public int CompareTo(object obj)
-		{
-			return alarmTime.CompareTo((obj as Alarm).AlarmTime);
-		}
-	}
 }
